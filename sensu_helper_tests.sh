307dbc0688c893bd194148f57b601170bd1a7333
Kyle Anderson <kyle@xkyle.com>
2013-12-15 01:55:44 +0400

    Fix whitespace and make it return 1 on anything non-zero

--- sensu_helper_tests.sh
+++ sensu_helper_tests.sh
@@ -9,8 +9,8 @@ function test_bad_option() (
 function test_with_default_args_in_a_simple_case() (
 shouldbe='{
 "name": "/bin/true",
-"output": "", 
-"status": "0"
+"output": "",
+"status": 0
 }'
   [[ "$(./sensu-shell-helper -d /bin/true 2>&1)" == "$shouldbe" ]]
 )
@@ -18,8 +18,8 @@ shouldbe='{
 function test_failing_output() (
 shouldbe='{
 "name": "/bin/false",
-"output": "", 
-"status": "1"
+"output": "",
+"status": 1
 }'
   [[ "$(./sensu-shell-helper -d /bin/false 2>&1)" == "$shouldbe" ]]
 )
@@ -27,8 +27,8 @@ shouldbe='{
 function test_with_output() (
 shouldbe='{
 "name": "/bin/echo test",
-"output": "test", 
-"status": "0"
+"output": "test",
+"status": 0
 }'
   [[ "$(./sensu-shell-helper -d /bin/echo test 2>&1)" == "$shouldbe" ]]
 )
@@ -37,8 +37,8 @@ shouldbe='{
 function test_with_hyphens() (
 shouldbe='{
 "name": "/bin/echo test",
-"output": "test", 
-"status": "0"
+"output": "test",
+"status": 0
 }'
   [[ "$(./sensu-shell-helper -d -- /bin/echo test 2>&1)" == "$shouldbe" ]]
 )
@@ -46,9 +46,9 @@ shouldbe='{
 function test_with_handlers() (
 shouldbe='{
 "name": "/bin/echo test",
-"output": "test", 
+"output": "test",
 "handlers": ["email", "pagerduty"],
-"status": "0"
+"status": 0
 }'
   RESULT=`./sensu-shell-helper -d -H '["email", "pagerduty"]' -- /bin/echo test 2>&1`
   [[ "$RESULT" == "$shouldbe" ]]
@@ -57,8 +57,8 @@ shouldbe='{
 function test_alternate_name() (
 shouldbe='{
 "name": "Name Override",
-"output": "", 
-"status": "0"
+"output": "",
+"status": 0
 }'
   RESULT=`./sensu-shell-helper -d -n "Name Override"  /bin/true 2>&1`
   [[ "$RESULT" == "$shouldbe" ]]
@@ -67,9 +67,9 @@ shouldbe='{
 function test_with_extra_JSON() (
 shouldbe='{
 "name": "/bin/echo test",
-"output": "test", 
+"output": "test",
 "metric": false
-"status": "0"
+"status": 0
 }'
   RESULT=`./sensu-shell-helper -d -j '"metric": false' -- /bin/echo test 2>&1`
   [[ "$RESULT" == "$shouldbe" ]]
@@ -80,8 +80,8 @@ shouldbe='{
 "name": "/usr/bin/seq 1 5",
 "output": "3
 4
-5", 
-"status": "0"
+5",
+"status": 0
 }'
   RESULT=`./sensu-shell-helper -d /usr/bin/seq 1 5 2>&1`
   [[ "$RESULT" == "$shouldbe" ]]
@@ -94,10 +94,30 @@ shouldbe='{
 97
 98
 99
-100", 
-"status": "0"
+100",
+"status": 0
 }'
   RESULT=`./sensu-shell-helper -d -c 5 /usr/bin/seq 1 100 2>&1`
+  [[ "$RESULT" == "$shouldbe" ]]
+)
+
+function test_non_nagios_compliant() (
+shouldbe='{
+"name": "exit 42",
+"output": "",
+"status": 1
+}'
+  RESULT=`./sensu-shell-helper -d -- exit 42 2>&1`
+  [[ "$RESULT" == "$shouldbe" ]]
+)
+
+function test_nagios_compliant() (
+shouldbe='{
+"name": "exit 42",
+"output": "",
+"status": 42
+}'
+  RESULT=`./sensu-shell-helper -N -d -- exit 42 2>&1`
   [[ "$RESULT" == "$shouldbe" ]]
 )
 
